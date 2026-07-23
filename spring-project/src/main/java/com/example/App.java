6791a0df2fc69ba97fb12c7b45f5f277051701bf
Laura Muresan <lauramuresan420@gmail.com>
2026-07-23 14:55:27 +0300

    Refactor application to use Java-based configuration

--- spring-project/src/main/java/com/example/App.java
+++ spring-project/src/main/java/com/example/App.java
@@ -1,10 +1,11 @@
 package com.example;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import org.springframework.context.annotation.Configuration;
+
+
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Hello hello = context.getBean("helloID", Hello.class);
+        Hello hello = new Hello();
         hello.sayHello();
         
     }
--- spring-project/src/main/java/com/example/Hello.java
+++ spring-project/src/main/java/com/example/Hello.java
@@ -1,7 +1,11 @@
 package com.example;
 
-public class Hello {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class Hello {
+    @Bean
     public void sayHello() {
         System.out.println("Hello, Spring!");
     }
