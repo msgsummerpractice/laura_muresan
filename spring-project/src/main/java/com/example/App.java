ff78e7a919cdd09478ee905e5cf5269a9c6e3329
Laura Muresan <lauramuresan420@gmail.com>
2026-07-23 16:37:07 +0300

    Create beans
    Create beans and annotate them with @Component, @Autowired and @Qualifier and a demonstration of their purpose.

--- spring-project/src/main/java/com/example/App.java
+++ spring-project/src/main/java/com/example/App.java
@@ -10,9 +10,12 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "com.example")
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(Hello.class);
-        Hello hello = context.getBean(Hello.class);
-        System.out.println(hello.sayHello());
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+       // Hello hello = context.getBean(Hello.class);
+
+        Folder folder = context.getBean(Folder.class);
+        //System.out.println(hello.sayHello());
+        folder.init();
         ((AnnotationConfigApplicationContext) context).close();
     }
 }
