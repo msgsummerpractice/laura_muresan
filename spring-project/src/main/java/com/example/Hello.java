d4e5f21bdb0ac14501786f7949e8e70762379208
Laura Muresan <lauramuresan420@gmail.com>
2026-07-24 09:52:21 +0300

    Fix Bean Annotations

--- spring-project/src/main/java/com/example/Hello.java
+++ spring-project/src/main/java/com/example/Hello.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import org.springframework.stereotype.Component;
-@Component
 public class Hello {
     public String sayHello() {
         return "Hello, Spring!";
--- spring-project/src/main/java/com/example/HelloConfiguration.java
+++ spring-project/src/main/java/com/example/HelloConfiguration.java
@@ -1,10 +1,11 @@
 package com.example;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloConfiguration {
-
+@Bean
     public Hello hello() {
         return new Hello();
     }
