564ee758034cfc193abeda1c01a5d2bca7c13af0
Laura Muresan <lauramuresan420@gmail.com>
2026-07-24 11:44:20 +0300

    Fix

--- spring-project/src/main/java/com/example/File.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-
-public class File {
-    public void init() {
-        System.out.println("File is initialized.");
-    }
-}
--- spring-project/src/main/java/com/example/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class Hello {
-    public String sayHello() {
-        return "Hello, Spring!";
-    }
-}
