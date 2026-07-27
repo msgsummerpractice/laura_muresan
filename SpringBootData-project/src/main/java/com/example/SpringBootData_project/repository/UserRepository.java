150477f89c924ad443fb3a781dc54e25b2d46296
Laura Muresan <lauramuresan420@gmail.com>
2026-07-27 13:26:39 +0300

    Writes two query methods one using JPA and a custom one using @Query

--- SpringBootData-project/src/main/java/com/example/SpringBootData_project/repository/UserRepository.java
+++ SpringBootData-project/src/main/java/com/example/SpringBootData_project/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.SpringBootData_project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.example.SpringBootData_project.model.User;
@@ -12,5 +14,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     public Optional<User> findByEmail(String email);
     public Optional<User> findByFirstName(String firstName);
 
-    
+    public List<User> findTop10ByFirstNameIgnoreCaseOrderByFirstNameAsc();
+
+    @Query("SELECT COUNT(*) FROM User")
+    public long countUsers();
 }
