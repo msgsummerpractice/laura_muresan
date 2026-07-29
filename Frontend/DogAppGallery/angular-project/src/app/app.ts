404acf1a2bd8c33752b0923da237004fa27212c9
Laura Muresan <lauramuresan420@gmail.com>
2026-07-29 17:21:25 +0300

    Addes toolbar

--- Frontend/DogAppGallery/angular-project/src/app/app.ts
+++ Frontend/DogAppGallery/angular-project/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('angular-project');
-}
+export class App {}
